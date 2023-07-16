60564aef5251a0207cc022a726d20080cdbcf2fd
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2023-07-15 23:46:16 -0700

    refactoring

--- server.js
+++ server.js
@@ -8,75 +8,37 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.use(async (req, res, next) => {
-  // authentication & set up
-  const auth = new google.auth.GoogleAuth({
-    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
+// authentication & set up
+const auth = new google.auth.GoogleAuth({
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+// set up connection with sheet api
+let sheets;
+(async function initializeGoogleSheets() {
   const client = await auth.getClient();
-  const sheets = google.sheets({ version: "v4", auth: client });
+  sheets = google.sheets({ version: "v4", auth: client });
+})();
+
+// MAIN MIDDLEWARE
+app.use("/:sheetName", async (req, res, next) => {
+  const { sheetName } = req.params;
+  console.log(sheetName);
 
   try {
     const sheetId = process.env.SHEET_ID;
-    const sheetName = req.originalUrl; // get resource name - this becomes sheet name
-    console.log(sheetName);
     const countCell = `${sheetName}!B1`;
 
-    // Check if the sheet exists
-    const sheetResponse = await sheets.spreadsheets.get({
-      spreadsheetId: sheetId,
-    });
-    const sheetExists = sheetResponse.data.sheets.some(
-      (sheet) => sheet.properties.title === sheetName
-    );
-
+    // create sheet if it doesn't exit
+    const sheetExists = await checkIfSheetExists(sheetId, sheetName);
     if (!sheetExists) {
-      // Create a new sheet
-      const createSheetResponse = await sheets.spreadsheets.batchUpdate({
-        spreadsheetId: sheetId,
-        requestBody: {
-          requests: [
-            {
-              addSheet: {
-                properties: {
-                  title: sheetName,
-                },
-              },
-            },
-          ],
-        },
-      });
-
-      // fill in fields
-      sheets.spreadsheets.values.update({
-        spreadsheetId: process.env.SHEET_ID,
-        range: `${sheetName}!A1:B1`,
-        valueInputOption: "USER_ENTERED", // how input data should be interpreted
-        resource: {
-          values: [["View Count:", "0"]], // the new incremented value
-        },
-      });
+      await createSheet(sheetId, sheetName);
     }
 
-    // read view count
-    const countResponse = await sheets.spreadsheets.values.get({
-      spreadsheetId: sheetId,
-      range: countCell,
-    });
-
-    // update view count
-    const viewCount = Number(countResponse.data.values[0][0]) + 1; // log sheet data
-
-    // write new view count
-    response = await sheets.spreadsheets.values.update({
-      spreadsheetId: process.env.SHEET_ID,
-      range: countCell,
-      valueInputOption: "USER_ENTERED", // how input data should be interpreted
-      resource: {
-        values: [[viewCount.toString()]], // the new incremented value
-      },
-    });
+    // read & update view count
+    const viewCount = await getViewCount(sheetId, countCell);
+    await updateViewCount(sheetId, countCell, viewCount + 1);
   } catch (err) {
     console.log("The API returned an error: " + err);
   }
@@ -84,10 +46,66 @@ app.use(async (req, res, next) => {
   next();
 });
 
-app.get("/test", (req, res) => {
-  res.send('<img src="/image"/>');
+app.get("/:all", (req, res) => {
+  res.send('<img src="/blank.svg"/>');
 });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+
+async function checkIfSheetExists(sheetId, sheetName) {
+  const sheetResponse = await sheets.spreadsheets.get({
+    spreadsheetId: sheetId,
+  });
+
+  return sheetResponse.data.sheets.some(
+    (sheet) => sheet.properties.title === sheetName
+  );
+}
+
+async function createSheet(sheetId, sheetName) {
+  await sheets.spreadsheets.batchUpdate({
+    spreadsheetId: sheetId,
+    requestBody: {
+      requests: [
+        {
+          addSheet: {
+            properties: {
+              title: sheetName,
+            },
+          },
+        },
+      ],
+    },
+  });
+
+  await sheets.spreadsheets.values.update({
+    spreadsheetId: sheetId,
+    range: `${sheetName}!A1:B2`,
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [["View Count:", "0"]],
+    },
+  });
+}
+
+async function getViewCount(sheetId, countCell) {
+  const countResponse = await sheets.spreadsheets.values.get({
+    spreadsheetId: sheetId,
+    range: countCell,
+  });
+
+  return Number(countResponse.data.values[0][0]);
+}
+
+async function updateViewCount(sheetId, countCell, newCount) {
+  await sheets.spreadsheets.values.update({
+    spreadsheetId: sheetId,
+    range: countCell,
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[newCount.toString()]],
+    },
+  });
+}
